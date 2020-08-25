b34203c6e8ab641ecac9c56c76673c522f7663b6
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-08-25 14:45:17 -0400

    Configure boot splash logo
    This assumes LOGO_BMP is set by the derivation as a make flag.

--- include/configs/roc-pc-rk3399.h
+++ include/configs/roc-pc-rk3399.h
@@ -6,10 +6,26 @@
 #ifndef __ROC_PC_RK3399_H
 #define __ROC_PC_RK3399_H
 
+// The following bunch of defines is for the splash
+#define CONFIG_SPLASHIMAGE_GUARD
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_SPLASH_SCREEN_ALIGN
+#define CONFIG_SPLASH_SOURCE
+#define CONFIG_SYS_VIDEO_LOGO_MAX_SIZE (1920*1080*4)
+#define CONFIG_VIDEO_BMP_GZIP
+#define CONFIG_VIDEO_BMP_LOGO
+#define CONFIG_VIDEO_BMP_RLE8
+#define CONFIG_VIDEO_LOGO
+#define SPLASH_ENV \
+		"splashimage=0x08080000\0" \
+		"splashpos=m,m\0" \
+		"splashsource=mmc_fs\0"
+
 // This sets up additional environment variables for our opinionated setup.
 // (See rk3399_common.h, it should be apended to ROCKCHIP_DEVICE_SETTINGS here
 //  but it makes patches harder to apply...)
 #define OPINIONATED_ENV \
+		SPLASH_ENV \
 		"bootmenu_delay=-1\0" \
 		"bootmenu_0=Default U-Boot boot=run distro_bootcmd; echo \"Boot failed.\"; sleep 5; $menucmd -1\0" \
 		"bootmenu_1=Boot from eMMC=run bootcmd_mmc0; echo \"eMMC Boot failed.\"; sleep 5; $menucmd -1\0" \
