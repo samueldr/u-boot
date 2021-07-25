c8780b089eac3aa744ef3477e3238b1c08139a02
George Hilliard <thirtythreeforty@gmail.com>
2021-07-25 19:16:22 -0400

    sunxi: Don't provide enable_cache() on suniv
    The arm926 code provides this function.  Disable it here.
    
    Signed-off-by: George Hilliard <thirtythreeforty@gmail.com>
    Signed-off-by: Yifan Gu <me@yifangu.com>

--- arch/arm/mach-sunxi/board.c
+++ arch/arm/mach-sunxi/board.c
@@ -406,7 +406,7 @@ void reset_cpu(void)
 #endif
 }
 
-#if !CONFIG_IS_ENABLED(SYS_DCACHE_OFF) && !defined(CONFIG_ARM64)
+#if !CONFIG_IS_ENABLED(SYS_DCACHE_OFF) && !defined(CONFIG_ARM64) && !defined(CONFIG_CPU_ARM926EJS)
 void enable_caches(void)
 {
 	/* Enable D-cache. I-cache is already enabled in start.S */
