5c14b9d6c4e32d60ed5a0e23ba13c3a0384e0911
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-04-18 21:43:00 -0400

    WIP: spl: Rework SPL_RAM support to be more generic
    The main reason behind this work, is to try to straighten out the
    spaghetti that the function had become.
    
    The function was used to uphold different load methods that are not
    actually 100% related, other than by the fact that they use data found
    in RAM. A tenuous link, since the next stage is highly likely to be
    found in RAM at some point during the lifetime of U-Boot.
    
    The methods this upheld were (non-exhaustively):
    
     - Loading next stage from SPL image data
     - Loading next stage from DFU-loaded information
     - Loading next stage from binman referenced data
     - Loading next stage from data already in RAM
    
    And using the data in these ways:
    
     - Using location as a FIT image
     - Using location as Legacy image
    
    A nuance to consider here his that these methods each tried to use the
    data in slightly divergent ways. For example loading the data from a
    FIT image only was possible if the SPL itself is a FIT image, and from
    the SPL itself.
    
    This, instead, uses any plausible location, from the previous schemes,
    and tries all of the different schemes at those locations.
    
    With these changes, it is possible to use `binman` to build a non-FIT
    SPL image (e.g. for Rockchip systems), which includes the unmodified
    U-Boot proper FIT image, which is referenced through its binman symbol.
    
    In this exact situation, allowing U-Boot proper to run entirely from
    “maskrom” mode using `rkdeveloptool` or `rockusb`. No traces of U-Boot
    to be found on any storage medium.

--- common/spl/spl_ram.c
+++ common/spl/spl_ram.c
@@ -16,86 +16,125 @@
 #include <spl.h>
 #include <linux/libfdt.h>
 
-static ulong spl_ram_load_read(struct spl_load_info *load, ulong sector,
-			       ulong count, void *buf)
+/*
+ * SPL loader for spl_ram.
+ */
+static ulong spl_ram_load_read(struct spl_load_info *load, ulong sector, ulong count, void *buf)
 {
-	ulong addr = 0;
-
-	debug("%s: sector %lx, count %lx, buf %lx\n",
-	      __func__, sector, count, (ulong)buf);
-
-	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT)) {
-		addr = IF_ENABLED_INT(CONFIG_SPL_LOAD_FIT,
-				      CONFIG_SPL_LOAD_FIT_ADDRESS);
-	}
-	addr += sector;
-	if (CONFIG_IS_ENABLED(IMAGE_PRE_LOAD))
-		addr += image_load_offset;
-
-	memcpy(buf, (void *)addr, count);
+	ulong address = (ulong)load->priv;
+	debug("\t%s: address 0x%lx, sector 0x%lx, count 0x%lx, buf 0x%lx\n", __func__, address, sector, count, (ulong)buf);
+	address += sector;
+	debug("\t%s: actual address = 0x%lx\n", __func__, address);
+	memcpy(buf, (void *)address, count);
 
 	return count;
 }
 
-static int spl_ram_load_image(struct spl_image_info *spl_image,
-			      struct spl_boot_device *bootdev)
+/*
+ * Tries to load the next stage from the given address.
+ *
+ * First, handles IMAGE_PRE_LOAD, if relevant.
+ *
+ * Then, tries to use the location as either a FIT image, or a Legacy image.
+ */
+static int spl_ram_try_location(struct spl_image_info *spl_image, struct spl_boot_device *bootdev, ulong address)
 {
-	struct legacy_img_hdr *header;
-	ulong addr = 0;
-	int ret;
+	int ret = 0;
 
-	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT)) {
-		addr = IF_ENABLED_INT(CONFIG_SPL_LOAD_FIT,
-				      CONFIG_SPL_LOAD_FIT_ADDRESS);
-	}
+	debug("\t%s: address = 0x%lx\n", __func__, address);
 
 	if (CONFIG_IS_ENABLED(IMAGE_PRE_LOAD)) {
-		ret = image_pre_load(addr);
+		debug("\t%s: Running IMAGE_PRE_LOAD.\n", __func__);
+		ret = image_pre_load(address);
 
-		if (ret)
+		if (ret) {
+			debug("\t%s: IMAGE_PRE_LOAD ret = %d.\n", __func__, ret);
 			return ret;
+		}
 
-		addr += image_load_offset;
+	 	address += image_load_offset;
 	}
-	header = map_sysmem(addr, 0);
 
-#if CONFIG_IS_ENABLED(DFU)
-	if (bootdev->boot_device == BOOT_DEVICE_DFU)
-		spl_dfu_cmd(0, "dfu_alt_info_ram", "ram", "0");
-#endif
+	struct legacy_img_hdr *header;
+	header = map_sysmem(address, 0);
 
-	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT) &&
-	    image_get_magic(header) == FDT_MAGIC) {
+	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT) && image_get_magic(header) == FDT_MAGIC) {
 		struct spl_load_info load;
 
-		debug("Found FIT\n");
-		spl_load_init(&load, spl_ram_load_read, NULL, 1);
+		debug("\t%s: Found a FIT image.\n", __func__);
+		// NOTE: passing the address itself, and not a pointer.
+		spl_load_init(&load, spl_ram_load_read, (void*)address, 1);
 		ret = spl_load_simple_fit(spl_image, &load, 0, header);
 	} else {
-		ulong u_boot_pos = spl_get_image_pos();
-
-		debug("Legacy image\n");
-		/*
-		 * Get the header.  It will point to an address defined by
-		 * handoff which will tell where the image located inside
-		 * the flash.
-		 */
-		debug("u_boot_pos = %lx\n", u_boot_pos);
-		if (u_boot_pos == BINMAN_SYM_MISSING) {
-			/*
-			 * No binman support or no information. For now, fix it
-			 * to the address pointed to by U-Boot.
-			 */
-			u_boot_pos = (ulong)spl_get_load_buffer(-sizeof(*header),
-								sizeof(*header));
-		}
-		header = map_sysmem(u_boot_pos, 0);
-
+		debug("\t%s: Not a FIT image. Trying Legacy image\n", __func__);
+		header = map_sysmem(address, 0);
 		ret = spl_parse_image_header(spl_image, bootdev, header);
 	}
 
 	return ret;
 }
+
+/*
+ * Glue logic that allowing different methods of locating the next stage in RAM.
+ *
+ * First, handles DFU upload to RAM, to support DFU from SPL.
+ * It is expected that SPL DFU support uploads to one of the locations tried next.
+ *
+ * Then, tries the different plausible locations the next stage may be;
+ * from the SPL image itself, from a reference from binman, or from where the next stage is expected to be loaded into.
+ *
+ * All of these methods try to go to the next stage using the same function (`spl_ram_try_location`).
+ * This means that any of the supported methods to load U-Boot will be tried at every plausible locations.
+ */
+static int spl_ram_load_image(struct spl_image_info *spl_image, struct spl_boot_device *bootdev)
+{
+	ulong address = -1;
+	int ret;
+
+	if (CONFIG_IS_ENABLED(DFU) && bootdev->boot_device == BOOT_DEVICE_DFU) {
+		debug("\t%s: Loading data from DFU.\n", __func__);
+		spl_dfu_cmd(0, "dfu_alt_info_ram", "ram", "0");
+		// NOTE: the spl_dfu implementation assumes the following attempts will try the dfu_alt_info_ram locations.
+	}
+
+	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT)) {
+		debug("\t%s: Trying from CONFIG_SPL_LOAD_FIT_ADDRESS.\n", __func__);
+		address = IF_ENABLED_INT(CONFIG_SPL_LOAD_FIT, CONFIG_SPL_LOAD_FIT_ADDRESS);
+		ret = spl_ram_try_location(spl_image, bootdev, address);
+		debug("\t >> ret = %d\n", ret);
+	}
+
+	if (ret != 0) {
+		debug("\t%s: Trying from a u-boot-any ref from current SPL image.\n", __func__);
+		address = spl_get_image_pos();
+		if (address != BINMAN_SYM_MISSING) {
+			ret = spl_ram_try_location(spl_image, bootdev, address);
+			debug("\t >> ret = %d\n", ret);
+		}
+		else {
+			debug("\t%s: Warning: No binman information for a 'u-boot-any' in current SPL image!!\n", __func__);
+		}
+	}
+
+	/* No binman support or no information. */
+	if (ret != 0) {
+		struct legacy_img_hdr *header;
+		header = map_sysmem(address, 0);
+		/* For now, fix it to the address pointed to by U-Boot. */
+		debug("\t%s: Falling back to SPL load buffer...\n", __func__);
+		/*
+		 * Get the header.  It will point to an address defined by
+		 * handoff which will tell where the image located inside
+		 * the flash.
+		 */
+		address = (ulong)spl_get_load_buffer(-sizeof(*header), sizeof(*header));
+		ret = spl_ram_try_location(spl_image, bootdev, address);
+		debug("\t >> ret = %d\n", ret);
+	}
+
+	return ret;
+}
+
 #if CONFIG_IS_ENABLED(RAM_DEVICE)
 SPL_LOAD_IMAGE_METHOD("RAM", 0, BOOT_DEVICE_RAM, spl_ram_load_image);
 #endif
