c7b49740ec85cf05d1161662d2783f9e99482af0
Samuel Dionne-Riel <samuel@dionne-riel.com>
2026-01-06 17:33:34 -0500

    XXX broken

--- default.nix
+++ default.nix
@@ -7,6 +7,16 @@
 }:
 
 let
+  this_src = 
+    # Strip this file from the source.
+    # We want this file in the repository, sadly this doesn't work well with builtins.fetchGit.
+    builtins.path {
+      name = "source";
+      path = (builtins.fetchGit ./.);
+      filter = path: type: baseNameOf path != "default.nix";
+    }
+  ;
+
   escape = builtins.toJSON;
   platFor =
     attr:
@@ -19,27 +29,28 @@ let
   ;
   plats = {
     "aarch64-linux" = pkgs.pkgsCross.aarch64-multiplatform;
+    "armv7l-linux" = pkgs.pkgsCross.armv7l-hf-multiplatform;
   };
   u-boot-for =
-    attr:
-    (platFor attr).${attr}
-    .overrideAttrs (oldAttrs: {
-      version = "master@2025-04-18";
-      src =
-        # Strip this file from the source.
-        # We want this file in the repository, sadly this doesn't work well with builtins.fetchGit.
-        builtins.path {
-          name = "source";
-          path = (builtins.fetchGit ./.);
-          filter = path: type: baseNameOf path != "default.nix";
-        }
+    attrOrDrv:
+    let
+      drv =
+        if builtins.isString attrOrDrv then
+          (platFor attrOrDrv).${attrOrDrv}
+        else
+          attrOrDrv
       ;
+    in
+    drv.overrideAttrs (oldAttrs: {
+      version = "master@2025-04-18";
+      src = this_src;
       postInstall = (oldAttrs.postInstall or "") + ''
         cp -v .config $out/config
       '';
     })
   ;
-  make-rk3588-u-boot =
+
+  make-64bit-rockchip-u-boot =
     attr: cfg:
     (u-boot-for attr).overrideAttrs (oldAttrs: {
       extraConfig = ''
@@ -86,6 +97,7 @@ in
     # $ rkdeveloptool db .../result/rk3588_combined_loader.bin
     # $ cargo run --features="libusb" --example rockusb download-boot .../result/rk3588_combined_loader.bin
     #
+    # FIXME: add per-SoC knowledge.
     make-maskrom-uploadable =
       { u-boot }:
       pkgs.callPackage (
@@ -96,9 +108,10 @@ in
         }:
         runCommand "${u-boot.defconfig}-maskrom-uploadable" {
           src = rkbin.src;
+          # 3568
           ini = ''
             [CHIP_NAME]
-            NAME=RK3588
+            NAME=RK3568
             [VERSION]
             MAJOR=1
             MINOR=11
@@ -106,7 +119,7 @@ in
             # Data sent to SRAM, to initialize SDRAM and continue in CODE472
             [CODE471_OPTION]
             NUM=1
-            Path1=${rkbin.TPL_RK3588}
+            Path1=${rkbin.TPL_RK3568}
             Sleep=1
 
             # Data sent to SDRAM
@@ -121,10 +134,10 @@ in
             null=/dev/null
 
             [OUTPUT]
-            PATH=rk3588_combined_loader.bin
+            PATH=rk3568_combined_loader.bin
 
             #
-            # RK3588 settings
+            # RK3568 settings
             #
             [SYSTEM]
             NEWIDB=true
@@ -133,6 +146,102 @@ in
             471_RC4_OFF=true
             RC4_OFF=true
           '';
+          ## 3588
+          #ini = ''
+          #  [CHIP_NAME]
+          #  NAME=RK3588
+          #  [VERSION]
+          #  MAJOR=1
+          #  MINOR=11
+
+          #  # Data sent to SRAM, to initialize SDRAM and continue in CODE472
+          #  [CODE471_OPTION]
+          #  NUM=1
+          #  Path1=${rkbin.TPL_RK3588}
+          #  Sleep=1
+
+          #  # Data sent to SDRAM
+          #  [CODE472_OPTION]
+          #  NUM=1
+          #  Path1=${u-boot}/u-boot-rockchip-maskrom.bin
+
+          #  # This section needs to exist.
+          #  [LOADER_OPTION]
+          #  NUM=1
+          #  LOADER1=null
+          #  null=/dev/null
+
+          #  [OUTPUT]
+          #  PATH=rk3588_combined_loader.bin
+
+          #  #
+          #  # RK3588 settings
+          #  #
+          #  [SYSTEM]
+          #  NEWIDB=true
+          #  # Disables(?) RC4 on DDR and Loader entries(?)
+          #  [FLAG]
+          #  471_RC4_OFF=true
+          #  RC4_OFF=true
+          #'';
+          ##ini = ''
+          ##  [CHIP_NAME]
+          ##  NAME=RV1126
+          ##  [VERSION]
+          ##  MAJOR=1
+          ##  MINOR=5
+          ##  [CODE471_OPTION]
+          ##  NUM=1
+          ##  Path1=${/Users/samuel/SBCs/rockchip/rkbin/bin/rv11/rv1126_ddr_924MHz_v1.14.bin}
+          ##  Sleep=1
+          ##  [CODE472_OPTION]
+          ##  NUM=1
+          ##  Path1=${u-boot}/u-boot-rockchip-maskrom.bin
+
+          ##  ### [CHIP_NAME]
+          ##  ### NAME=RV1126
+          ##  ### [VERSION]
+          ##  ### MAJOR=1
+          ##  ### MINOR=5
+          ##  ### #[CHIP_NAME]
+          ##  ### #NAME=RK3588
+          ##  ### #[VERSION]
+          ##  ### #MAJOR=1
+          ##  ### #MINOR=11
+
+          ##  ### # Data sent to SRAM, to initialize SDRAM and continue in CODE472
+          ##  ### [CODE471_OPTION]
+          ##  ### NUM=1
+          ##  ### #Path1=${rkbin.TPL_RK3588}
+          ##  ### #Path1=${u-boot}/u-boot-tpl.bin
+          ##  ### Path1=${/Users/samuel/SBCs/rockchip/rkbin/bin/rv11/rv1126_ddr_924MHz_v1.14.bin}
+          ##  ### #
+          ##  ### Sleep=1
+
+          ##  ### # Data sent to SDRAM
+          ##  ### [CODE472_OPTION]
+          ##  ### NUM=1
+          ##  ### Path1=${u-boot}/u-boot-rockchip-maskrom.bin
+
+          ##  # This section needs to exist.
+          ##  [LOADER_OPTION]
+          ##  NUM=1
+          ##  LOADER1=null
+          ##  null=/dev/null
+
+          ##  [OUTPUT]
+          ##  PATH=rv1126_combined_loader.bin
+
+          ##  ### # #
+          ##  ### # # RK3588 settings
+          ##  ### # #
+          ##  ### # [SYSTEM]
+          ##  ### # NEWIDB=true
+          ##  ### # # Disables(?) RC4 on DDR and Loader entries(?)
+          ##  ### # [FLAG]
+          ##  ### # 471_RC4_OFF=true
+          ##  ### # RC4_OFF=true
+          ##'';
           passAsFile = [ "ini" ];
         } ''
         cp --recursive --target-directory . "$src"/{tools,bin,RKBOOT}
@@ -161,13 +270,30 @@ in
       }
     ;
 
-    radxa-rock5b = make-rk3588-u-boot "ubootRock5ModelB" {};
+    radxa-rock5b = make-64bit-rockchip-u-boot "ubootRock5ModelB" {};
     radxa-rock5b-maskrom-uploadable = make-maskrom-uploadable {
       u-boot = radxa-rock5b;
     };
 
     # NOTE: still borrow's Nixpkgs' ubootRock5ModelB as a starting point.
-    generic-rk3588 = (make-rk3588-u-boot "ubootRock5ModelB" {}).override {
+    generic-rk3568 = ((make-64bit-rockchip-u-boot "ubootRock5ModelB" {}).override {
+      defconfig = "generic-rk3568_defconfig";
+      filesToInstall = [
+        "u-boot.itb"
+        "idbloader.img"
+        "u-boot-rockchip.bin"
+      ];
+    }).overrideAttrs(_: {
+      ROCKCHIP_TPL = pkgs.rkbin.TPL_RK3566;
+    });
+    #  $ $(nix-build --no-out-link --attr rockchiprs)/bin/rockusb download-boot $(nix-build --no-out-link --attr generic-rk3568-maskrom-uploadable)/*.bin
+    # ```
+    generic-rk3568-maskrom-uploadable = make-maskrom-uploadable {
+      u-boot = generic-rk3568;
+    };
+
+    # NOTE: still borrow's Nixpkgs' ubootRock5ModelB as a starting point.
+    generic-rk3588 = (make-64bit-rockchip-u-boot "ubootRock5ModelB" {}).override {
       defconfig = "generic-rk3588_defconfig";
       filesToInstall = [
         "u-boot.itb"
@@ -181,6 +307,79 @@ in
     generic-rk3588-maskrom-uploadable = make-maskrom-uploadable {
       u-boot = generic-rk3588;
     };
+
+
+    ## NOTE: borrowing an aarch64 rockchip build still...
+    #generic-rv1126 = (make-64bit-rockchip-u-boot "ubootRock64" {}).override {
+    #  defconfig = "generic-rv1126_defconfig";
+    #  filesToInstall = [
+    #    "u-boot.itb"
+    #    "idbloader.img"
+    #    "u-boot-rockchip.bin"
+    #  ];
+    #  meta.platforms = [
+    #    "armv7l-linux"
+    #  ];
+    #};
+
+    generic-rv1126 =
+      let
+        drv =
+          pkgs.pkgsCross.armv7l-hf-multiplatform.buildUBoot {
+            #defconfig = "generic-rv1126_defconfig";
+            #defconfig = "sonoff-ihost-rv1126_defconfig";
+            defconfig = "neu2-io-rv1126_defconfig";
+            extraConfig = ''
+              CONFIG_SPL_RAM_DEVICE=y
+              CONFIG_SPL_RAM_SUPPORT=y
+            '';
+            extraMeta.platforms = [ "armv7l-linux" ];
+            filesToInstall = [
+              #"u-boot.itb"
+              "idbloader.img"
+              "u-boot-rockchip.bin"
+              "spl/u-boot-spl.bin"
+              #"tpl/u-boot-tpl.bin"
+              "u-boot-rockchip-maskrom.bin"
+            ];
+            # XXX probably broken?
+            # But where would I get the TEE?
+            TEE="/dev/null";
+            #TEE=/Users/samuel/SBCs/rockchip/rkbin/bin/rv11/rv1126_tee_ta_v2.16.bin;
+            ROCKCHIP_TPL=/Users/samuel/SBCs/rockchip/rkbin/bin/rv11/rv1126_ddr_924MHz_v1.14.bin;
+          }
+        ;
+      in
+      u-boot-for drv
+    ;
+    generic-rv1126-maskrom-uploadable = make-maskrom-uploadable {
+      u-boot = generic-rv1126;
+    };
+
+    #sonoff-ihost-rv1126 =
+    #  let
+    #    drv =
+    #      pkgs.pkgsCross.armv7l-hf-multiplatform.buildUBoot {
+    #        defconfig = "sonoff-ihost-rv1126_defconfig";
+    #        #extraConfig = ''
+    #        #  CONFIG_SPL_RAM_DEVICE=y
+    #        #  CONFIG_SPL_RAM_SUPPORT=y
+    #        #'';
+    #        extraMeta.platforms = [ "armv7l-linux" ];
+    #        filesToInstall = [
+    #          "u-boot.itb"
+    #          "idbloader.img"
+    #          "u-boot-rockchip.bin"
+    #          "spl/u-boot-spl.bin"
+    #          #"u-boot-rockchip-maskrom.bin"
+    #        ];
+    #        TEE="/dev/null";
+    #      }
+    #    ;
+    #  in
+    #  u-boot-for drv
+    #;
+
 
     inherit pkgs;
   }
